18c82b187e7632ca77c46e9c9fcc1d230d8ee6e9
ShahMdMahi <shahmdmahi13@gmail.com>
2024-05-18 21:12:06 +0600

    update 3

--- src/components/BannerHome.js
+++ src/components/BannerHome.js
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { FaAngleRight, FaAngleLeft } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const BannerHome = () => {
 
@@ -72,7 +73,9 @@ const BannerHome = () => {
                       <span>|</span>
                       <p>View: {Number(data.popularity).toFixed(0)}+</p>
                     </div>
-                    {/* <button className='bg-white px-4 py-2 text-black font-bold rounded mt-3 hover:bg-gradient-to-l from-red-700 to-orange-500 shadow-md transition-all hover:scale-105'>Play Now</button> */}
+                    <Link to={"/" + data?.media_type + "/" + data?.id} >
+                      <button className='bg-white px-4 py-2 text-black font-bold rounded mt-4 hover:bg-gradient-to-l from-red-700 to-orange-500 shadow-md transition-all hover:scale-105'>Play Now</button>
+                    </Link>
                   </div>
                 </div>
 
